926bf1a859613cbb9b2ecb493a20aa9cce0dda14
Juha Kukkonen <juha@juhaku.ddns.net>
2018-03-19 22:54:27 +0200

    build change

--- integrity.sh
+++ integrity.sh
@@ -16,6 +16,7 @@ currentVersion=$(grep version gradle.properties | sed s/.*=//)
 if [ "$1" == "release" ]; then
     updateVersion $(echo $currentVersion | sed "s/-SNAPSHOT//")
     rm -rf out/make
+    rm -rf build
     yarn electron-forge make
     git commit -am 'Gradle Release: Automatic version upgrade' && git push
 elif [ "$1" == "rollback" ]; then
