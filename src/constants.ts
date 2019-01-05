5f2c6c05b889b96e3ee260bb97deff65d236b28f
Juha Kukkonen <juha7kukkonen@gmail.com>
2019-01-05 17:05:15 +0200

    [Automated release commit] next version: 1.1.1-SNAPSHOT

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ declare var __static: string;
 export default class Constants {
     public static readonly BASE_PATH = process.env.NODE_ENV === 'development'
         ? __static.replace('/public', '') : __static.replace('/resources/app', '').replace('.asar', '');
-    public static readonly VERSION = '1.1.0';
+    public static readonly VERSION = '1.1.1-SNAPSHOT';
     public static readonly LATEST_RELEASE_URL = 'http://api.github.com/repos/juhaku/loginized/releases/latest';
     public static readonly IS_DEBUG = process.env.NODE_ENV === 'development';
 }
